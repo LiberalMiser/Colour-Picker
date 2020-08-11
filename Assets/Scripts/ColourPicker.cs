107f107d29068af7b2004e4fdc384ab7ae599c2b
LiberalMiser <31970701+LiberalMiser@users.noreply.github.com>
2020-08-11 06:21:02 -0400

    Refactored code, sliders now begin at the correct position
    1) Got rid of unnecessary code in the script
    
    2) Added functionality to have the colour sliders and metallic and smoothness sliders begin at the correct positions
    
    3) Minor adjustments to things in the scene

--- Assets/Scripts/ColourPicker.cs
+++ Assets/Scripts/ColourPicker.cs
@@ -4,93 +4,65 @@ using System.Collections;
 
 public class ColourPicker : MonoBehaviour {
 
-    [Header("Hue:")]
-    public Image hueImage;
-    public Slider hueSlider;
-    public InputField hueInputField;
-    [Header("Saturation:")]
-    public Image saturationImage;
-    public Slider saturationSlider;
-    public InputField saturationInputField;
-    [Header("Lightness Value:")]
-    public Image valueLightnessImage;
-    public Slider valueLightnessSlider;
-    public InputField valueInputField;
-    [Header("Settings:")]
-    public int multiply = 540;
-	public Material carMat;
+    [SerializeField] private Image saturationImage;
+    [SerializeField] private Material targetMaterial;
     [SerializeField] private string metallicKey = "_Metallic";
     [SerializeField] private string smoothnessKey = "_Glossiness";
     [SerializeField] private string colourKey = "_BaseColor";
+    [SerializeField] private Slider hueSlider, valueLightnessSlider, saturationSlider, metallicSlider, smoothnessSlider;
+    [SerializeField] private bool useHDR = true;
 
-    private float saturation;
-    private GameObject currentVehicle;
-    private float valueLightness;
-    private float hue;
-    private Renderer carRenderer;
-    private Shader currentShader;
+    private float hue, saturation, colourValue;
 
-    private void Start () {
-        SetSaturationBarColour();
-
-        hue = hueSlider.value;
-        saturation = saturationSlider.value;
-        valueLightness = valueLightnessSlider.value;
+    private void Awake() {
+        InitializeColourSliderPositions();
+        InitializeSmoothnessSliderPosition();
+        InitializeMetallicSliderPosition();
     }
 
-    public void SetSaturationBarColour () {
-        int xCord = (int)(hueSlider.value * multiply);
-        saturationImage.color = hueImage.sprite.texture.GetPixel(xCord, 0);
+    public void SetColour() {
+        Color color = Color.HSVToRGB(hue, saturation, colourValue, useHDR);
+        targetMaterial.SetColor(colourKey, color);
+        saturationImage.color = color;
     }
 
-    /*private int SaturationBarColour () {
-        int xCord = (int)(hueSlider.value * multiply);
-        saturationImage.color = hueImage.sprite.texture.GetPixel(xCord, 0);
-        return xCord;
-    }*/
-
-    public void SetHue (float _hue) {
-        hue = _hue;
-        hueInputField.text = hue.ToString();
+    public void SetHue(float val) {
+        hue = val;
     }
 
-    public void SetSaturation (float _value) {
-        saturation = _value;
-        saturationInputField.text = saturation.ToString();
+    public void SetSaturation(float val) {
+        saturation = val;
     }
 
-    public void SetValueLightness (float _value) {
-        valueLightness = _value;
-        valueInputField.text = valueLightness.ToString();
+    public void SetColourValue(float val) {
+        colourValue = val;
     }
 
-    public void SetColour () {
-        //carMat.color = Color.HSVToRGB(hue, saturation, valueLightness, true);
-        carMat.SetColor(colourKey, Color.HSVToRGB(hue, saturation, valueLightness, true));
+    public void SetMetallicValue(float val) {
+        targetMaterial.SetFloat(metallicKey, val);
     }
 
-    public void ChangeMetalness (float _value) {
-        carMat.SetFloat(metallicKey, _value);
+    public void SetSmoothnessValue(float val) {
+        targetMaterial.SetFloat(smoothnessKey, val);
     }
 
-    public void ChangeSmoothness (float _value) {
-        carMat.SetFloat(smoothnessKey, _value);
+    private void InitializeColourSliderPositions() {
+        Color materialStartColour = targetMaterial.GetColor(colourKey);
+        float h, s, v;
+
+        Color.RGBToHSV(materialStartColour, out h, out s, out v);
+
+        hueSlider.value = h;
+        saturationSlider.value = s;
+        valueLightnessSlider.value = v;
     }
 
-    public void UpdateHueSlider () {
-        hueSlider.value = float.Parse(hueInputField.text);
+    private void InitializeSmoothnessSliderPosition() {
+        smoothnessSlider.value = targetMaterial.GetFloat(smoothnessKey);
     }
 
-    public void UpdateSaturationSlider(float _value) {
-        saturationSlider.value = float.Parse(saturationInputField.text);
-    }
-
-    public void UpdateValueLightnessSlider(float _value) {
-        valueLightnessSlider.value = float.Parse(valueInputField.text);
-    }
-
-    public void SetColourByHex (string hex) {
-        
+    private void InitializeMetallicSliderPosition() {
+        metallicSlider.value = targetMaterial.GetFloat(metallicKey);
     }
 
 }
